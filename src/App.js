34eb5828e54b91edb1a03a1ac320913689e12b35
Thiago-Jeager <santiago16waly@gmail.com>
2024-09-13 22:39:40 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React La mano de dios
         </a>
       </header>
     </div>
